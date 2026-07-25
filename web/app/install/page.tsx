b5dc1e0397aa1de06a5af58378e407c1a18f3615
abhay <rohitlodhi0701@gmail.com>
2026-07-25 21:17:32 +0530

    fix build issues on web

--- web/app/install/page.tsx
+++ web/app/install/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "motion/react";
 import CopyButton from "@/components/CopyButton";
 import StepNumber from "@/components/StepNumber";
